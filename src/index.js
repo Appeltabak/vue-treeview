b399735ea65f90fbe637db17bfadaf348230af8a
Tonnis Oosterhoff <tonnisoosterhoff@gmail.com>
2018-02-05 21:35:55 +0100

    Fixed folding / unfolding toggle

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Vue from "vue";
 
 Vue.component('tree-view-list', {
     template: `
     <ul>
-        <li v-for="child in children">
-            <span>{{ child.name }}</span>
-            <tree-view-list v-if="child.children" :children="child.children"></tree-view-list>
+        <li v-for="child in children" :key="child.name" style="list-style: none;">
+            <span v-on:click="toggle">{{ child.name }}</span>
+            <tree-view-list v-show="showChildren" :children="child.children"></tree-view-list>
         </li>
     </ul>
     `,
+    data() {
+        return {
+            showChildren: false
+        }
+    },
 
     props: {
         children: Array
+    },
+
+    methods: {
+        toggle: function(event) {
+            if(event){
+                this.showChildren = !this.showChildren && this.children.length > 0
+            }
+        }
     }
 })
 
@@ -25,9 +39,15 @@ export default new Vue({
                     name: 'Zone 1',
                     children: [{
                         name: 'Foot Zone 1',
-                        children: undefined
+                        children: []
+                    }, {
+                        name: 'Foot Zone 2',
+                        children: []
                     }]
                 }]
+            }, {
+                name: 'Ahu 2',
+                children: []
             }
         ]
     }
